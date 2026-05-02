131036829d8a046ac44de826c6239bbd51b63431
matiasabdiasromero2008-del <matiasabdiasromero2008@gmail.com>
2026-05-02 22:27:47 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import uvicorn
 if __name__ == "__main__":
     print("Iniciando Rinde ERP Web Server...")
     print("Por favor, abre tu navegador en: http://localhost:8000")
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
